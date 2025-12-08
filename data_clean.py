c7463b90034463b016d74621cf2703e32aae4893
Yarik <you@example.comyarikvitovsky@gmail.com>
2025-12-08 06:55:23 +0200

    final version

--- data_clean.py
+++ data_clean.py
@@ -13,7 +13,12 @@ data['Age'] = data['Age'].fillna(data['Age'].mean())
 missing_values = data.isnull().sum()
 print(missing_values)
 
-scaled_data = scaler.fit_transform(data) #NumPy Object
-data = pd.DataFrame(scaled_data, columns=data.columns) #converting back to data frame
+target = data['Survived']
+features = data.drop(columns=['Survived'])
+
+scaled_features = scaler.fit_transform(features)#NumPy Object
+scaled_features = pd.DataFrame(scaled_features, columns=features.columns)#converting back to data frame
+
+data = pd.concat([scaled_features, target], axis=1)
 
 data.to_csv('data/train_cleaned.csv', index=False)
